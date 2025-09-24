798bce9b6e41f151a0c62e0c2225f3555276f511
Yerko Sepulveda Rojas <yerko.sepulveda@pregrado.uoh.cl>
2025-09-24 15:10:15 -0300

    fix url

--- WatchTask/vite.config.js
+++ WatchTask/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/WatchTask/",
+  base: "/WatchTaskV2/",
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 5 * 1024 * 1024,
