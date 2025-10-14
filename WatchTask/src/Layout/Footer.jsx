f5383898d2e67080a96459158d2b81306c3e6187
Yerko Sepulveda Rojas <yerko.sepulveda@pregrado.uoh.cl>
2025-10-14 17:20:28 -0300

    .

--- WatchTask/src/Layout/Footer.jsx
+++ WatchTask/src/Layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-white shadow-inner mt-auto">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 text-center text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} WatchTask. v0.0.1.7 Debug State
+        &copy; {new Date().getFullYear()} WatchTask. v0.1 production stable
       </div>
     </footer>
   );
